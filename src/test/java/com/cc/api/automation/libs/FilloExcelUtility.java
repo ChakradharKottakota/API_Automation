b1d0c5e58b80076ab145b48f263bfc481db0bc03
Chakradhar Kottakota <RLE0210@rlpl.com>
2018-04-04 12:18:49 +0530

    path changes

--- src/test/java/com/cc/api/automation/libs/FilloExcelUtility.java
+++ src/test/java/com/cc/api/automation/libs/FilloExcelUtility.java
@@ -22,7 +22,7 @@ public class FilloExcelUtility {
 	
 	
 	/***For  Linux***/
-	static String  exceldbloc="/var/lib/jenkins/jobs/API_Automation/src/test/resources/files/ConfigSheet.xlsx";
+	static String  exceldbloc="/var/lib/jenkins/workspace/API_Automation/src/test/resources/files/ConfigSheet.xlsx";
 
 	/*For client linux env*/
 	//	static String  exceldbloc="/home/genadmin/workspace/Genesis_API_Automation_new/src/test/resources/files/ConfigSheet.xlsx";
